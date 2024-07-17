7c3a2b27149619e4e595cbc4e71e6e3737c83dc4
sbrizu <solaramibrizuela@gmail.com>
2024-07-17 15:24:41 -0400

    Create bienvenidosdisplay.ino

---
+++ bienvenidosdisplay.ino
@@ -0,0 +1,20 @@
+#include <Adafruit_SSD1306.h>
+Adafruit_SSD1306 display = Adafruit_SSD1306(128, 64, &Wire);
+
+void setup() {
+
+  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C); // Address 0x3C for 128x32
+
+  display.display();
+  delay(2000);
+
+  display.clearDisplay();      //this line to clear previous logo
+  display.setTextColor(WHITE); //without this no display
+  display.print("Bienvenidos");//your TEXT here
+  display.display();            //to shows or update your TEXT
+}
+
+void loop() {
+
+ }
